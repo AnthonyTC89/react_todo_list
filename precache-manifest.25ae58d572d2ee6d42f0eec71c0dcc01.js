55530b40cf8b563ec2553de7df1c8f89127f9bf5
AnthonyTC89 <ptonyptc@gmail.com>
2019-12-24 18:16:40 -0500

    Updates

---
+++ precache-manifest.25ae58d572d2ee6d42f0eec71c0dcc01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72f2e86290e29e1be1446d0daa501355",
+    "url": "/AnthonyTC89/react_todo_list/index.html"
+  },
+  {
+    "revision": "f8b552dccf459b2e6dc4",
+    "url": "/AnthonyTC89/react_todo_list/static/css/main.060170ec.chunk.css"
+  },
+  {
+    "revision": "b04f32ddc68ff7ae6ef4",
+    "url": "/AnthonyTC89/react_todo_list/static/js/2.4e40d5c4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/AnthonyTC89/react_todo_list/static/js/2.4e40d5c4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f8b552dccf459b2e6dc4",
+    "url": "/AnthonyTC89/react_todo_list/static/js/main.86bbdb62.chunk.js"
+  },
+  {
+    "revision": "e319fc4b38b856feb537",
+    "url": "/AnthonyTC89/react_todo_list/static/js/runtime-main.76127747.js"
+  }
+]);
